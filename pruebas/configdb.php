f38b6979707ea0e75f677724d688a3021ba27a8a
Fran <contratop@molecule.net>
2026-03-19 19:02:31 +0100

    los php subios

---
+++ pruebas/configdb.php
@@ -0,0 +1,7 @@
+<?php
+// Esto es el ejemplo de una conexion local en xampp
+define('SERVIDOR', 'localhost');
+define('USUARIO', 'root');
+define('PASSWORD', '');
+define('BBDD', 'pruebas');
+?>
---
+++ pruebas/mostrar_datos.php
@@ -0,0 +1,22 @@
+<?php
+require 'configdb.php';
+function conectar()
+{
+    $db = new mysqli(SERVIDOR, USUARIO, PASSWORD, BBDD);
+    if ($db->connect_error) {
+        die("Error de conexión: " . $db->connect_error);
+    }
+    $db->set_charset('utf8');
+    return $db;
+}
+
+function mostrar_datos()
+{
+    $db = conectar();
+    $sql = 'SELECT * from alumnos';
+    $resultado = $db->query($sql);
+    $fila = $resultado->fetch_array();
+    echo 'Nombre alumno: ' . $fila['nombre'];
+    $db->close();
+}
+mostrar_datos();
