45362aee8477d3d3b80f7f51d32cee443c08e97a
Fran <franciscojaviermedinagordillo.guadalupe@alumnado.fundacionloyola.net>
2026-03-23 14:18:07 +0100

    login a medias (aun)

---
+++ pruebas/login.php
@@ -0,0 +1,31 @@
+<?php
+require 'pruebas/configdb.php';
+function conectar()
+{
+    $db = new mysqli(SERVIDOR, USUARIO, PASSWORD, BBDD);
+    if ($db->connect_error) {
+        die("Error de conexión: " . $db->connect_error);
+    }
+    $db->set_charset('utf8');
+    return $db;
+}
+
+
+
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+$sql = "SELECT idALumno FROM alumno
+WHERE username='" . $username . "'
+AND password='" . $password . "'";
+
+
+
+
+$result = $db->query($sql);
+if ($result->num_rows > 0) {
+    $fila = $result->fetch_array();
+}
+
+?>
