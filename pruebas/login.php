94ca468e78c8d5af8f06e1d69b7da64acf804880
Fran <franciscojaviermedinagordillo.guadalupe@alumnado.fundacionloyola.net>
2026-03-24 09:43:05 +0100

    aclaracion

--- pruebas/login.php
+++ pruebas/login.php
@@ -25,13 +25,19 @@ AND passwd='" . $password . "'";
 $result = $db->query($sql);
 
 if ($result && $result->num_rows > 0) {
+    // ESTA COMPROBACION ES LA CORRECTA PARA EVITAR CRASHES EN PHP
+    // AUNQUE NO SE HAYA DADO EN CLASE
+    // explicación:
+    // Si solo se pone $result->num_rows > 0 y $result es NULL PHP CRASHEA
+    // De lo contrario, si se pone $result en un AND, en caso de que la consulta SQL
+    // no funcionase correctamente, seria false y ya nisiquiera llega a comprobar el num_rows
+    // No se puede llamar a num_rows si $result es NULL
     $fila = $result->fetch_array();
     $_SESSION['id_usuario'] = $fila['id'];
     $_SESSION['nombre_usuario'] = $fila['nombre'];
     header("Location: ../bienvenida.php");
     exit();
-}
-else {
+} else {
     header("Location: ../login_error.html");
     exit();
 }
